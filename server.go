4d195802ab0795d242faf0d023d06fd09c174072
Christian <3003html@gmail.com>
2022-06-13 18:50:07 -0500

    Function server listen added

--- server.go
+++ server.go
@@ -1,5 +1,7 @@
 package main
 
+import "net/http"
+
 type Server struct {
 	port string
 }
@@ -8,4 +10,12 @@ func NewServer(port string) *Server {
 	return &Server{
 		port: port,
 	}
+}
+
+func (s *Server) Listen() error {
+	err := http.ListenAndServe(s.port, nil)
+	if err != nil {
+		return err
+	}
+	return nil
 }
