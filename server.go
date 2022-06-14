60c85ab8da70eb272eff3b1aa3b0be28eda1296e
Christian <3003html@gmail.com>
2022-06-13 20:00:40 -0500

    Comments added to file server.go

--- server.go
+++ server.go
@@ -2,17 +2,21 @@ package main
 
 import "net/http"
 
+// Struct of Server
 type Server struct {
 	port   string
 	router *Router
 }
 
+// Function to create a new server
 func NewServer(port string) *Server {
 	return &Server{
 		port:   port,
 		router: NewRouter(),
 	}
 }
+
+// Methods
 
 func (s *Server) Listen() error {
 	http.Handle("/", s.router)
