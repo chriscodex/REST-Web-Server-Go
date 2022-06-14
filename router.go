b3693ae70fba64bf09da7a6db5329fc5bae2e681
Christian <3003html@gmail.com>
2022-06-13 19:37:42 -0500

    Method ServeHTTP added

--- router.go
+++ router.go
@@ -1,13 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type Router struct {
 	rules map[string]http.HandlerFunc
 }
 
+// Methods
+
 func NewRouter() *Router {
 	return &Router{
 		rules: make(map[string]http.HandlerFunc),
 	}
+}
+
+// This function turns Router into a Handler(Interface)
+func (r *Router) ServeHTTP(w http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(w, "Server Working")
 }
