bfa45e0b229801382f734570765bc77d8bfa3880
Christian <3003html@gmail.com>
2022-06-14 17:55:03 -0500

    Home endpoint created with his handler

--- handler.go
+++ handler.go
@@ -7,5 +7,10 @@ import (
 
 // Handle to manage the principal root ("/")
 func HandlerRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Root working")
+	fmt.Fprintf(w, "Root endpoint working")
+}
+
+// Handle to manage home
+func HandleHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home endpoint working")
 }
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 func main() {
 	server := NewServer(":3000")
 	server.Handle("/", HandlerRoot)
+	server.Handle("/home", HandleHome)
 	server.Listen()
 }
