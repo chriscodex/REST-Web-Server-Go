fd01a9d659eb0bba0350d422ae1c071d022c9243
Christian <3003html@gmail.com>
2022-06-15 15:05:00 -0500

    Update handler to manage post request in user endpoint and create endpoint deleted

--- handler.go
+++ handler.go
@@ -16,18 +16,6 @@ func HandlerHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "API endpoint working")
 }
 
-// Receive a json and decode it
-func PostRequest(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var metadata Metadata
-	err := decoder.Decode(&metadata)
-	if err != nil {
-		fmt.Fprintf(w, "error: %v", err)
-		return
-	}
-	fmt.Fprintf(w, "Payload %v\n", metadata)
-}
-
 // Handler to manage the post request in user endpoint
 func UserPostRequest(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 func main() {
 	server := NewServer(":3000")
 	server.Handle("/", "GET", HandlerRoot)
-	server.Handle("/create", "POST", PostRequest)
 	server.Handle("/user", "POST", UserPostRequest)
 	server.Handle("/api", "POST", server.AddMiddleware(HandlerHome, CheckAuth(), Loggin()))
 	server.Listen()
